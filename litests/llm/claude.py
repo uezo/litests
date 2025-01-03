0ef115fca24256094062a0dfc1ff8ed9faec672c
uezo <uezo@uezo.net>
2025-01-03 14:41:27 +0900

    Support Gemini, Claude, Dify, and others through LiteLLM unified interface
    Enabled support for various LLMs, including Gemini, Claude, and Dify. Additionally, through LiteLLM, you can access any other LLM APIs supported by LiteLLM.

--- litests/llm/claude.py
+++ litests/llm/claude.py
@@ -1,20 +1,21 @@
 from logging import getLogger
 from typing import AsyncGenerator, Dict, List
-import openai
+from anthropic import AsyncAnthropic
 from . import LLMService
 
 logger = getLogger(__name__)
 
 
-class ChatGPTService(LLMService):
+class ClaudeService(LLMService):
     def __init__(
         self,
         *,
-        openai_api_key: str = None,
+        anthropic_api_key: str = None,
         system_prompt: str = None,
         base_url: str = None,
-        model: str = "gpt-4o",
+        model: str = "claude-3-5-sonnet-latest",
         temperature: float = 0.5,
+        max_tokens: int = 1024,
         split_chars: List[str] = None,
         option_split_chars: List[str] = None,
         option_split_threshold: int = 50,
@@ -29,13 +30,15 @@ class ChatGPTService(LLMService):
             option_split_threshold=option_split_threshold,
             skip_before=skip_before
         )
-        self.openai_client = openai.AsyncClient(api_key=openai_api_key, base_url=base_url)
+        self.anthropic_client = AsyncAnthropic(
+            api_key=anthropic_api_key,
+            base_url=base_url
+        )
+        self.max_tokens = max_tokens
         self.contexts: List[Dict[str, List]] = {}
 
     def compose_messages(self, context_id: str, text: str) -> List[dict]:
         messages = []
-        if self.system_prompt:
-            messages.append({"role": "system", "content": self.system_prompt})
         messages.extend(self.contexts.get(context_id, []))
         messages.append({"role": "user", "content": text})
         return messages
@@ -47,13 +50,13 @@ class ChatGPTService(LLMService):
         self.contexts[context_id] = messages
 
     async def get_llm_stream_response(self, context_id: str, messages: List[dict]) -> AsyncGenerator[str, None]:
-        stream_resp = await self.openai_client.chat.completions.create(
+        async with self.anthropic_client.messages.stream(
             messages=messages,
+            system=self.system_prompt or "",
             model=self.model,
             temperature=self.temperature,
-            stream=True
-        )
-
-        async for chunk in stream_resp:
-            if content := chunk.choices[0].delta.content:
-                yield content
+            max_tokens=self.max_tokens
+        ) as stream_resp:
+            async for chunk in stream_resp.text_stream:
+                if content := chunk:
+                    yield content
